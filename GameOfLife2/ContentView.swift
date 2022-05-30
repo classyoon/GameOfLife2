578bb04758fcc53d58e68478c9727b4cdce6dbb0
Conner Yoon <con.yoon@icloud.com>
2022-05-29 23:33:33 -0400

    Initial Commit

--- GameOfLife2/ContentView.swift
+++ GameOfLife2/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct Cow {
+    
+}
+
+struct Tile : Identifiable {
+    var color : Color {
+        Color.gray
+    }
+    var id = UUID()
+}
+class BoardVM : ObservableObject {
+    @Published var tiles : [[Tile]]
+    static let RowMax = 10
+    static let ColMax = 4
+    
+    init(){
+        tiles = Array(repeating: Array(repeating: Tile(), count: BoardVM.ColMax), count: BoardVM.RowMax)
+    }
+}
+struct BoardView : View {
+    @ObservedObject var boardVM = BoardVM()
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<BoardVM.RowMax, id: \.self) { row in
+                HStack {
+                    ForEach(0..<BoardVM.ColMax, id: \.self) { col in
+                        boardVM.tiles[row][col].color
+                    }
+                }
+                
+            }
+        }
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        BoardView()
     }
 }
 
