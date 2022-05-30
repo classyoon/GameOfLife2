b684de8086a85531820807ba688da2fcfaf89a0d
Conner Yoon <con.yoon@icloud.com>
2022-05-29 22:52:53 -0400

    Initial Commit

---
+++ GameOfLife2/GameOfLife2App.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLife2App.swift
+//  GameOfLife2
+//
+//  Created by Conner Yoon on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLife2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
